3e4663c01ec67dc8cb916b75fbac2b805f89f65a
Isaac <isaac.yonemoto@gmail.com>
2017-12-19 00:29:10 -0800

    fix a few things including relative path for the project root

--- lib/mix/tasks/swagger.ex
+++ lib/mix/tasks/swagger.ex
@@ -1,8 +1,6 @@
 defmodule Mix.Tasks.Swagger do
   use Mix.Task
 
-  @project_root File.cwd!
-
   @shortdoc "generates an api from the Exaggerate task"
   def run(swaggerfile) do
     swaggerfile |> Enum.map(&Exaggerate.Codesynth.build_fromfile/1)
