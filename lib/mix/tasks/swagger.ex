c208060250d3aa945db92312fc6a48cd878f3fb9
Isaac <isaac.yonemoto@gmail.com>
2017-12-20 20:49:40 -0800

    minor code cleanup

--- lib/mix/tasks/swagger.ex
+++ lib/mix/tasks/swagger.ex
@@ -5,14 +5,12 @@ defmodule Mix.Tasks.Swagger do
   def run(swaggerfile) do
     swaggerfile |> Enum.map(&Exaggerate.Codesynth.buildswaggerfile/1)
   end
-
 end
 
 defmodule Mix.Tasks.Swagger.Update do
   use Mix.Task
 
   @shortdoc "updates an api from the supplied swaggerfile(s)"
-
   def run(swaggerfile) do
     swaggerfile |> Enum.map(&Exaggerate.Codesynth.updateswaggerfile/1)
   end
