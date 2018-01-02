035161ae420bf2d9086b1c9e272bef57a537272a
Isaac <isaac.yonemoto@gmail.com>
2018-01-01 17:04:23 -0800

    reformat the output of input validation function so that it conforms to {:ok, val}

--- lib/exaggerate.ex
+++ lib/exaggerate.ex
@@ -1,10 +1,17 @@
 defmodule Exaggerate do
-  # NB: this is kind of a very lame way of doing this.  A way of autodetecting 
+  # NB: this is kind of a very lame way of doing this.  A way of autodetecting
   # if a library is in a context would be MUCH better.
   def get_project_root do
     case File.cwd! |> Path.split |> Enum.at(-2) do
       "deps" -> Path.relative_to_cwd("../../") |> Path.expand
       _ -> File.cwd!
+    end
+  end
+
+  def append_if_ok(test, val) do
+    case test do
+      :ok -> {:ok, val}
+      val -> val
     end
   end
 end
