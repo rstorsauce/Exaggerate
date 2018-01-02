7c384da618681a6253f4c5b6bc1efbbc4a8f2c76
Isaac <isaac.yonemoto@gmail.com>
2018-01-02 11:57:12 -0800

    put the filtering in the correct place

--- lib/codesynth/codesynth.ex
+++ lib/codesynth/codesynth.ex
@@ -31,12 +31,7 @@ defmodule Exaggerate.Codesynth do
   def get_defs([_head | tail], :no), do: get_defs(tail)
 
   def insert_code(new_functions, code_tokens) do
-    IO.puts("==================")
-    IO.puts(code_tokens |> Enum.join)
-    IO.puts("------------------")
-    new_functions = "\n" <> new_functions
-    new_code = Enum.slice(code_tokens, 0..-3) ++ [new_functions] ++ ["\n","end"] |> Enum.join
-    IO.puts(new_code)
+    new_code = Enum.slice(code_tokens, 0..-3) ++ ["\n" <> new_functions] ++ ["\n","end"] |> Enum.join
     new_code |> Code.format_string! |> Enum.join
   end
 
